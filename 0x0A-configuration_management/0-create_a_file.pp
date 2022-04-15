a614de3d6837ade46439b98a9aa41fbc96900716
Omar Cherni <89669057+OmarCherniXO@users.noreply.github.com>
2022-04-15 18:30:46 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # PUPPET SCRIPT THAT CREATES A FILE IN /tmp.
 file{'/tmp/school':
-permission => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet',
+  ensure  => 'file',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
