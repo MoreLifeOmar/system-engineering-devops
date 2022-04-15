fd8ab4c7dd6ab6d7eaae8358e931c8a206a5ab0d
OmarCherniXO <dreamdaddybanks@gmail.com>
2022-04-15 18:35:19 +0100

    PUPPET

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # PUPPET SCRIPT THAT CREATES A FILE IN /tmp.
 file{'/tmp/school':
   ensure  => 'file',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
